fca3598fb7a8384c8350efc5b6ded6961b7c47e9
Juan Álvarez Molina <alvarezmolinajuan@gmail.com>
2026-02-14 22:18:16 +0100

    delete

--- formbuilder/config.js
+++
@@ -1,4 +0,0 @@
-// FormBuilder Config - paste your token below
-const GITHUB_TOKEN_PART1 = "ghp_mMEPiZ5FaR";
-const GITHUB_TOKEN_PART2 = "O6Fbh9BmuvtuPkNKwzdE0zFV3H";
-const FB_GITHUB_TOKEN = GITHUB_TOKEN_PART1 + GITHUB_TOKEN_PART2;
